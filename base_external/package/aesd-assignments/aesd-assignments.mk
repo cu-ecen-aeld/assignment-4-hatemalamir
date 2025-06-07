e6976a38f37261079d3fbc21e16d0492d2dbb399
hatemalamir <hatem.alamir.90@gmail.com>
2025-06-07 11:01:38 -0400

    Fix aesd-assignments.mk to include latest commit from assignment 3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = d78b57a54020fe157d53b2885476fa308ef5f3f3
+AESD_ASSIGNMENTS_VERSION = 13bbb9d37712d196e274f7d47e832e4345de81f2
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
