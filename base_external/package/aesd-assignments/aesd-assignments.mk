b8c8bd562b7ea4ebe65e0f4f4db1752013e64e8f
hatemalamir <hatem.alamir.90@gmail.com>
2025-07-05 10:55:36 -0400

    Build aesdsocket with buildroot - Use latest commit from asgn 5

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = caa2c9d50a69e538f1d0eb8256da516be02fdf2f
+AESD_ASSIGNMENTS_VERSION = 996aaf97c69bb21f959fa8d817a2bf1f900b1b80
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
